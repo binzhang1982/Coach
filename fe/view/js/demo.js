36471ec89f3eacea7b16eac8644d456190886320
binzhang <bzhang@hpe.com>
2017-05-19 22:10:39 +0800

    safpuqweriou

--- fe/view/js/demo.js
+++ fe/view/js/demo.js
@@ -336,11 +336,50 @@ app.directive("rating", function() {
   return directive;
 });
 
+app.factory('netRequest', ['$q', '$http', 'Url', "$rootScope", function ($q, $http, Url, $rootScope) {
+  function handleResponse(promise) {
+    return promise.then(function (res) {
+      if (res.data.status == "ok")
+        return $q.resolve(res.data.data);
+      else if (res.data.status == "error" && res.data.data == "10000")
+        $.alert("没有当前操作权限!");
+      else
+        return $q.reject(res.data.message);
+    }, function (res) {
+      return $q.reject("服务器请求异常");
+    });
+  }
+
+  //设置10s请求超时
+  var timeout = 10000000;
+  var service = {};
+
+  service.saveStudent = function (student) {
+    var url = Url.saveStudent + "?token=" + $rootScope.token;
+    var promise = $http.post(url, student, {timeout: timeout});
+    return handleResponse(promise);
+  }
+
+  return service;
+
+}])
+  .factory('Url', [function () {
+    var host ="http://localhost:8080";
+    var path="/coach-talk/api";
+    return {
+      //获取服务清单列表
+      //getServiceList: host + path + "/custserv/servlist/{managingId}",
+      //保存学生
+      saveStudent: host + path + "/student/save_student"
+    };
+  }]);
+
+
 //
 // For this trivial demo we have just a unique MainController
 // for everything
 //
-app.controller('MainController', function($rootScope, $scope) {
+app.controller('MainController', ['$rootScope', '$scope', 'netRequest', function($rootScope, $scope, netRequest) {
 
   $scope.swiped = function(direction) {
     alert('Swiped ' + direction);
@@ -418,30 +457,40 @@ app.controller('MainController', function($rootScope, $scope) {
   ];
 
   $scope.examlevels = [
-    {id: 'A1', name: 'A1 大型载客汽车'},
-    {id: 'A2', name: 'A2 重型,中型全挂,半挂汽车列车'},
-    {id: 'A3', name: 'A3 核载10人以上的城市公共汽车'},
-    {id: 'B1', name: 'B1 中型载客汽车'},
-    {id: 'B2', name: 'B2 重型,中型载货汽车等'},
-    {id: 'C1', name: 'C1 小型,微型载客汽车等'},
-    {id: 'C2', name: 'C2 小型,微型自动挡载客汽车等'},
-    {id: 'C3', name: 'C3 低速载货汽车(原四轮农用运输车)'},
-    {id: 'C4', name: 'C4 三轮汽车(原三轮农用运输车)'},
-    {id: 'D', name: 'D 发动机排量大于50ml或者最大设计车速大于50km/h的三轮摩托车'},
-    {id: 'E', name: 'E 发动机排量大于50ml或者最大设计车速大于50km/h的二轮摩托车'},
-    {id: 'F', name: 'F 发动机排量小于等于50ml或者最大设计车速小于等于50km/h的摩托车'},
-    {id: 'M', name: 'M 轮式自行机械车'},
-    {id: 'N', name: 'N 无轨电车'},
-    {id: 'P', name: 'P 有轨电车'}
+    {id: '1', name: 'A1 大型载客汽车'},
+    {id: '2', name: 'A2 重型,中型全挂,半挂汽车列车'},
+    {id: '3', name: 'A3 核载10人以上的城市公共汽车'},
+    {id: '4', name: 'B1 中型载客汽车'},
+    {id: '5', name: 'B2 重型,中型载货汽车等'},
+    {id: '6', name: 'C1 小型,微型载客汽车等'},
+    {id: '7', name: 'C2 小型,微型自动挡载客汽车等'},
+    {id: '8', name: 'C3 低速载货汽车(原四轮农用运输车)'},
+    {id: '9', name: 'C4 三轮汽车(原三轮农用运输车)'},
+    {id: '10', name: 'D 发动机排量大于50ml或者最大设计车速大于50km/h的三轮摩托车'},
+    {id: '11', name: 'E 发动机排量大于50ml或者最大设计车速大于50km/h的二轮摩托车'},
+    {id: '12', name: 'F 发动机排量小于等于50ml或者最大设计车速小于等于50km/h的摩托车'},
+    {id: '13', name: 'M 轮式自行机械车'},
+    {id: '14', name: 'N 无轨电车'},
+    {id: '15', name: 'P 有轨电车'}
   ];
   //
   // 'Forms' screen
   //
-  $scope.rememberMe = true;
   $scope.email = 'me@example.com';
+  $scope.student = {};
 
   $scope.addstudent = function() {
-    alert('You added a student');
+    $scope.student.levelId = $scope.student.selectedLevel.id;
+    $scope.student.sexId = $scope.student.selectedSex.id;
+
+    netRequest.saveStudent($scope.student).then(function (res) {
+      if (res != null) {
+        $scope.student.password = '';
+        $scope.student.repassword = '';
+      }
+    }, function (res) {
+      alert(res);
+    });
   };
 
   $scope.login = function() {
@@ -449,4 +498,4 @@ app.controller('MainController', function($rootScope, $scope) {
   };
 
 
-});
+}]);
